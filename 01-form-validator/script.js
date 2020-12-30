e111bf712b2be558abde6284493ff5266268bb31
Alan Nguyen <tuan8101@gmail.com>
2020-12-29 20:48:21 -0700

    Create check input length function

--- 01-form-validator/script.js
+++ 01-form-validator/script.js
@@ -35,6 +35,23 @@ function checkRequired(inputArr) {
   });
 }
 
+// Check input length
+function checkLength(input, min, max) {
+  if (input.value.length < min) {
+    showError(
+      input,
+      `${getFieldName(input)} must be at least ${min} characters`
+    );
+  } else if (input.value.length > max) {
+    showError(
+      input,
+      `${getFieldName(input)} must be less than ${max} characters`
+    );
+  } else {
+    showSuccess(input);
+  }
+}
+
 // Get field name
 function getFieldName(input) {
   return input.id.charAt(0).toUpperCase() + input.id.slice(1);
